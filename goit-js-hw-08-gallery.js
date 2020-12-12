8601244da3fe8e625e5c2dd7879ba38d1f81766c
IvettaDashkova <ivettazakonno@gmail.com>
2020-12-13 01:54:37 +0200

    добавляет решение дополнительных условий задачи

--- goit-js-hw-08-gallery.js
+++ goit-js-hw-08-gallery.js
@@ -1,15 +1,23 @@
 import defaultExport, * as gallery from "./gallery-items.js";
-
+//переменные
 const galleryItems = gallery.default;
 const mainNodeGallery = document.querySelector(".gallery");
-const modalWindow = document.querySelector('.lightbox');
-const btnModalWindow = document.querySelector('button[data-action="close-lightbox"]');
+const modalWindow = document.querySelector(".lightbox");
+const btnModalWindow = document.querySelector(
+  'button[data-action="close-lightbox"]'
+);
 const imgModalWindow = document.querySelector(".lightbox__image");
+const overlay = document.querySelector(".lightbox__overlay");
+const arrayLightboxImg = [];
+let index;
 
-
+//обработчики
 function addItem(array, parent) {
-  const createItem = array.map((objectGallery) => shapeNode(objectGallery));
-    parent.append(...createItem);
+  const createItem = array.map((objectGallery) => {
+    arrayLightboxImg.push(objectGallery.original);
+    return shapeNode(objectGallery);
+  });
+  parent.append(...createItem);
 }
 
 function shapeNode(objectGallery) {
@@ -29,19 +37,65 @@ function shapeNode(objectGallery) {
   addTagLink.append(addTagImg);
   return addTagLi;
 }
-function openModal(event) { 
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-    imgModalWindow.setAttribute("src", event.target.dataset.source);
-    modalWindow.classList.add('is-open')
+function openModal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  imgModalWindow.setAttribute("src", event.target.dataset.source);
+  modalWindow.classList.add("is-open");
+  //слушатели модального окна
+  window.addEventListener("keydown", closeModalPressEsc);
+  window.addEventListener("keydown", leafToRight);
+  window.addEventListener("keydown", leafToLeft);
 }
-function closeModal(event) { 
-    modalWindow.classList.remove("is-open");
-    imgModalWindow.setAttribute('src', '');
+function closeModal(event) {
+  modalWindow.classList.remove("is-open");
+  imgModalWindow.setAttribute("src", "");
+  window.removeEventListener("keydown", closeModalPressEsc);
+  window.removeEventListener("keydown", leafToRight);
+  window.removeEventListener("keydown", leafToLeft);
+}
+function closeModalPressEsc(event) {
+  if (event.key === "Escape") {
+    closeModal(event);
+  }
+}
+function leafToRight(event) {
+  if (event.key === "ArrowRight") {
+    getIndex(arrayLightboxImg);
+    let res =
+      index !== arrayLightboxImg.length - 1
+        ? imgModalWindow.setAttribute("src", arrayLightboxImg[index + 1])
+        : imgModalWindow.setAttribute("src", arrayLightboxImg[0]);
+    return res;
+  }
+}
+function leafToLeft(event) {
+  if (event.key === "ArrowLeft") {
+    getIndex(arrayLightboxImg);
+    let res =
+      index !== 0
+        ? imgModalWindow.setAttribute("src", arrayLightboxImg[index - 1])
+        : imgModalWindow.setAttribute(
+            "src",
+            arrayLightboxImg[arrayLightboxImg.length - 1]
+          );
+    return res;
+  }
+}
+function getIndex(array) {
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] === imgModalWindow.getAttribute("src")) {
+      index = i;
+    }
+  }
+  return index;
 }
 
+//слушатели
+mainNodeGallery.addEventListener("click", openModal);
+btnModalWindow.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
 addItem(galleryItems, mainNodeGallery);
-mainNodeGallery.addEventListener('click', openModal)
-btnModalWindow.addEventListener('click', closeModal)
